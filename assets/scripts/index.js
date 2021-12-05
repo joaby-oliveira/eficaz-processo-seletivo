b225b3d2223e7b4fa0f0f89715511aeb0d92e63e
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-12-05 11:12:26 -0300

    feat: register users

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,18 +1,79 @@
-const registerButton = document.querySelector('#register')
-const usersViewButton = document.querySelector('#usersView')
+// Variables
+const inputs = document.querySelectorAll('.inputBox input')
+const form = document.querySelector('form')
 
+async function registerUser(data) {
 
+  const result = await fetch('https://estagio.eficazmarketing.com/api/user', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+
+  const test = await result.json()
+}
+
+// Handle submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const data = {}
+
+  inputs.forEach(input => {
+    data[input.id] = input.value
+  })
+
+  registerUser(data)
+})
+
+// Validating data and masks
+const masks = {
+  cep(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+  },
+
+  telefone(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+  },
+
+  numero(value) {
+    return value
+      .replace(/\D/g, '')
+  },
+
+  uf(value) {
+    return value
+      .replace(/[^a-z]/ig, '')
+      .toUpperCase()
+  }
+}
+
+inputs.forEach(input => {
+  const field = input.id
+  input.addEventListener('input', ({ target }) => {
+    console.log(target.value)
+    target.value = masks[field](target.value)
+  })
+})
 
 // Change between "pages" logic
 const register = document.querySelector('.register')
 const usersView = document.querySelector('.usersView')
 
-function showRegister(){
+const registerButton = document.querySelector('#register')
+const usersViewButton = document.querySelector('#usersView')
+
+function showRegister() {
   register.classList.remove('hide')
   usersView.classList.add('hide')
 }
 
-function showUsersView(){
+function showUsersView() {
   register.classList.add('hide')
   usersView.classList.remove('hide')
 }
